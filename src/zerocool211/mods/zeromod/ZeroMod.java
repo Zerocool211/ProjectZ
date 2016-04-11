e432b104e0ffd094fad76faf5ac9a834173f7f31
Zerocool211 <zerocool211x@gmail.com>
2016-04-11 21:18:06 +0000

    Added Init Methods
    Added preInit , init and postInit methods.

--- src/zerocool211/mods/zeromod/ZeroMod.java
+++ src/zerocool211/mods/zeromod/ZeroMod.java
@@ -1,8 +1,31 @@
 package zerocool211.mods.zeromod;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "zeromod", name = "Zero Mod", version = "1.0")
 public class ZeroMod {
 
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+	    //Item/Block init and registering
+    	//Config handling
+    }
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		//Proxy, TileEntity, entity, Gui and Packet Register
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+	
 }
