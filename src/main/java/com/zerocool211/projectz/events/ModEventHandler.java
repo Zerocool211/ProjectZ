102a7b7dec1cf94df4456c8b9b94c7fa81286e4e
Zerocool211 <zerocool211x@gmail.com>
2017-05-01 22:31:13 +0000

    Played With Some Events
    in order to explore what can be done with them for furture stuff.

--- src/main/java/com/zerocool211/projectz/events/ModEventHandler.java
+++ src/main/java/com/zerocool211/projectz/events/ModEventHandler.java
@@ -1,6 +1,122 @@
 package com.zerocool211.projectz.events;
 
+import com.zerocool211.projectz.common.ModItems;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.GameType;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.event.entity.player.PlayerPickupXpEvent;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+
 public class ModEventHandler
 {
-    //todo
+
+    //Commented Code Is For Testing Purposes Only.
+
+    @SubscribeEvent
+    public void entityJoinWorld(EntityJoinWorldEvent event)
+    {
+        //if(event.getEntity() instanceof EntityPlayer)
+        //{
+            //EntityPlayer player = (EntityPlayer) event.getEntity();
+            //player.inventory.addItemStackToInventory(new ItemStack(Items.NETHER_STAR, 1));
+            //if(event.getWorld().isRemote)
+            //{
+                //player.addChatComponentMessage(new TextComponentString("Welcome To This World..."));
+            //}
+        //}
+    }
+
+    @SubscribeEvent
+    public void livingHurt(LivingHurtEvent event)
+    {
+        //if(event.getSource().getEntity() instanceof EntityPlayer)
+        //{
+            //EntityPlayer player = (EntityPlayer) event.getSource().getEntity();
+            //if(player.getHeldItemMainhand() != null)
+            //{
+                //if(player.getHeldItemMainhand().getItem() == ModItems.fakeDiamond)
+                //{
+                    //event.setAmount(10);
+                    //event.getEntity().setFire(10);
+                //}
+            //}
+        //}
+    }
+
+    @SubscribeEvent
+    public void itemPickup(PlayerEvent.ItemPickupEvent event)
+    {
+        //if(event.pickedUp.getEntityItem().getItem() == Items.DIAMOND)
+        //{
+            //event.player.inventory.clearMatchingItems(Items.DIAMOND, 0, 1, null);
+            //event.player.inventory.addItemStackToInventory(new ItemStack(Items.COAL, 1));
+            //event.player.removeExperienceLevel(1);
+            //event.player.inventory.damageArmor(15.0F);
+            //event.player.addChatComponentMessage(new TextComponentString("Diamonds!"));
+            //event.player.setFire(5);
+            //event.player.inventory.clear();
+            //event.player.inventory.addItemStackToInventory(new ItemStack(Items.IRON_INGOT));
+        //}
+    }
+
+    @SubscribeEvent
+    public void livingUpdate(LivingEvent.LivingUpdateEvent event)
+    {
+        //if(event.getEntity() instanceof EntityPlayer)
+        //{
+            //EntityPlayer player = (EntityPlayer) event.getEntity();
+            //if(player.getHeldItemMainhand() != null)
+            //{
+                //if(player.getHeldItemMainhand().getItem() == Items.APPLE)
+                //{
+                    //player.setFire(1);
+                    //player.addExperienceLevel(1);
+                //}
+            //}
+
+        //}
+    }
+
+    @SubscribeEvent
+    public void itemTooltip(ItemTooltipEvent event)
+    {
+        //if(event.getItemStack().getItem() == Items.DIAMOND)
+        //{
+            //event.getToolTip().add("Real Deal");
+        //}
+    }
+
+    @SubscribeEvent
+    public void blockBreak(BlockEvent.BreakEvent event)
+    {
+        //event.setExpToDrop(10);
+        //if(event.getState().getBlock() == Blocks.DIRT)
+        //{
+            //event.setExpToDrop(10);
+            //BlockPos pos = event.getPos();
+            //event.getPlayer().setGameType(GameType.CREATIVE);
+            //event.getWorld().spawnEntityInWorld(new EntityZombie(event.getWorld())); //Does Not Work
+            //event.getWorld().spawnEntityInWorld(new EntityItem(event.getWorld(), pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Items.NETHER_STAR)));
+        //}
+    }
+
+    @SubscribeEvent
+    public void onPlayerPickupXP(PlayerPickupXpEvent event)
+    {
+        //event.getOrb().xpValue *= 2;
+    }
+
 }
