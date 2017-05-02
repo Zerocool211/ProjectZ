0176ea8aaaeb8452e879050f6cd3b2b6c81679d8
Zerocool211 <zerocool211x@gmail.com>
2017-05-02 13:37:36 +0000

    Added Base Tablet

--- src/main/java/com/zerocool211/projectz/common/ModItems.java
+++ src/main/java/com/zerocool211/projectz/common/ModItems.java
@@ -24,6 +24,7 @@ public class ModItems
     public static ItemArmor fakeDiamondChestplate;
     public static ItemArmor fakeDiamondLeggings;
     public static ItemArmor fakeDiamondBoots;
+    public static Item baseTablet;
 
     public static void preInit()
     {
@@ -37,6 +38,7 @@ public class ModItems
         fakeDiamondChestplate = register(new ModArmor(ProjectZ.FAKE_DIAMOND,EntityEquipmentSlot.CHEST, "fake_diamond_chestplate"));
         fakeDiamondLeggings = register(new ModArmor(ProjectZ.FAKE_DIAMOND,EntityEquipmentSlot.LEGS, "fake_diamond_leggings"));
         fakeDiamondBoots = register(new ModArmor(ProjectZ.FAKE_DIAMOND,EntityEquipmentSlot.FEET, "fake_diamond_boots"));
+        baseTablet = new ModItem("base_tablet");
 
         registerItems();
     }
@@ -49,6 +51,7 @@ public class ModItems
         register(fakeDiamondPickaxe, new ResourceLocation(ProjectZ.MODID, "fake_diamond_pickaxe"));
         register(fakeDiamondAxe, new ResourceLocation(ProjectZ.MODID, "fake_diamond_axe"));
         register(fakeDiamondHoe, new ResourceLocation(ProjectZ.MODID, "fake_diamond_hoe"));
+        register(baseTablet, new ResourceLocation(ProjectZ.MODID, "base_tablet"));
     }
 
     public static void registerRenders()
@@ -63,6 +66,7 @@ public class ModItems
         registerRender(fakeDiamondChestplate);
         registerRender(fakeDiamondLeggings);
         registerRender(fakeDiamondBoots);
+        registerRender(baseTablet);
     }
 
     public static void registerRender(Item item)
