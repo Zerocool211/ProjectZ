10e31941489941105957f5b8169399a61fabc85b
Zerocool211 <zerocool211x@gmail.com>
2017-05-04 19:22:05 +0000

    Added rightClick method to these classes

--- src/main/java/com/zerocool211/projectz/items/DayTablet.java
+++ src/main/java/com/zerocool211/projectz/items/DayTablet.java
@@ -1,11 +1,25 @@
 package com.zerocool211.projectz.items;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
 public class DayTablet extends ModItem
 {
 
     public DayTablet(String name)
     {
         super(name);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World world, EntityPlayer playerIn, EnumHand hand)
+    {
+        //stuff
+        return new ActionResult<>(EnumActionResult.SUCCESS, stack);
     }
 
 }
