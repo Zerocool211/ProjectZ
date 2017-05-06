3f95728347868c8558bffc4dac5db36abf7c9a75
Zerocool211 <zerocool211x@gmail.com>
2017-05-06 12:41:36 +0000

    Changed Methods For Time Tablets

--- src/main/java/com/zerocool211/projectz/items/TimeTablet.java
+++ src/main/java/com/zerocool211/projectz/items/TimeTablet.java
@@ -3,10 +3,9 @@ package com.zerocool211.projectz.items;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class TimeTablet extends ModItem
@@ -18,14 +17,14 @@ public class TimeTablet extends ModItem
     }
 
     @Override
-    public EnumActionResult onItemUse(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World world, EntityPlayer player, EnumHand hand)
     {
         if(player.inventory.hasItemStack(new ItemStack(Items.COAL)))
         {
             world.setWorldTime(world.getWorldTime() + 1000);
-            return EnumActionResult.SUCCESS;
+            return new ActionResult<>(EnumActionResult.SUCCESS, stack);
         }
-        return EnumActionResult.PASS;
+        return new ActionResult<>(EnumActionResult.PASS, stack);
     }
 
 }
